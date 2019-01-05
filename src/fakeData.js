6872c8702bdda6072509bdee4df8a205e8c03ed6
Leonardo Larrosa <llarrosasanchez@teco.com.ar>
2019-01-05 20:44:15 -0300

    refactor fakeData and services

--- src/fakeData.js
+++ src/fakeData.js
@@ -4,29 +4,6 @@ export default {
             title: 'Este es un Título',
             subTitle: 'Esto es un subtítulo',
             description: 'Esto es una descripción del artículo',
-            banner: "http://scd.es.rfi.fr/sites/espanol.filesrfi/dynimagecache/0/864/4896/2759/600/338/sites/images.rfi.fr/files/aef_image/img_20180313_1424497.jpg",
-            date: '2018-05-12',
-        },
-        {
-            title: 'Este es otro título',
-            subTitle: 'Este es otro subtítulo',
-            description: 'Esto es otra descripción del artículo',
-            banner: "https://www.thesprucepets.com/thmb/-dY_W4edPcFENqO9wH-tsIQeotc=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/37348687_179210919505845_8579658165484781568_n-5b69b1b346e0fb00500b0880.jpg",
-            date: '2018-03-06',
-        },
-        {
-            title: 'Este es un tercer título',
-            subTitle: 'Este es un tercer subtítulo',
-            description: 'Esto es una tercera descripción del artículo',
-            banner: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/galaxy-cat-universe-kitten-launch-------------johnnie-art.jpg",
-            date: '2018-04-07',
-        }
-    ],
-    fakeFetchContent: [
-        {
-            title: 'Este es un Título',
-            subTitle: 'Esto es un subtítulo',
-            description: 'Esto es una descripción del artículo',
             content: [
                 { type: 'ovTitle', value: 'Esto es un Título' },
                 { type: 'ovSubtitle', value: 'Esto es un Subtitle' },
@@ -40,15 +17,17 @@ export default {
                 { type: 'ovBlock', value: 'Esto es un bloque de texto.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut velit dignissim, efficitur dolor sit amet, dignissim nisl. Donec nec velit vel metus rutrum blandit vitae ac massa. Suspendisse ut congue massa, ac feugiat purus. Fusce scelerisque commodo lacus, ut sodales ipsum egestas non. Integer viverra metus velit, quis faucibus orci lacinia maximus. Sed sollicitudin, felis quis facilisis auctor, magna justo fermentum arcu, et sagittis ex ante quis mauris. Fusce viverra risus ut lacus cursus, et blandit nibh molestie. Quisque feugiat egestas nisl nec ultricies. Mauris sapien elit, pharetra id nisl eu, tristique venenatis purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer vel gravida nulla.' },
                 { type: 'ovFrame', value: 'Esto es un frame. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor consectetur justo. Etiam in facilisis purus. Sed placerat erat consequat, gravida turpis dignissim, faucibus velit.' },
                 { type: 'ovLink', value1: "https://www.google.com/", value2: 'Link' },
-                { type: 'ovPhoto', value1: "https://purrfectlove.net/wp-content/uploads/2018/01/gingercat.jpg", value2:'Soy un gato de barrio' },
-                { type: 'ovQuoteFirm', value1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor consectetur justo. Etiam in facilisis purus. Sed placerat erat consequat, gravida turpis dignissim, faucibus velit.', value2:'Nombre de Autor' },
+                { type: 'ovPhoto', value1: "https://purrfectlove.net/wp-content/uploads/2018/01/gingercat.jpg", value2: 'Soy un gato de barrio' },
+                { type: 'ovQuoteFirm', value1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor consectetur justo. Etiam in facilisis purus. Sed placerat erat consequat, gravida turpis dignissim, faucibus velit.', value2: 'Nombre de Autor' },
                 { type: 'ovSeparator' },
                 { type: 'ovImage', value: "https://www.imagenesdegatos.net/wp-content/uploads/2015/12/gato-gris-ojos-naranja-3.jpg" },
-                { type: 'ovDetail', value: [ 
-                    { label: 'Nombre', value: 'Leonardo' },
-                    { label: 'Apellido', value: 'Larrosa' },
-                    { label: 'Género', value: 'Masculino'}        
-                ]}
+                {
+                    type: 'ovDetail', value: [
+                        { label: 'Nombre', value: 'Leonardo' },
+                        { label: 'Apellido', value: 'Larrosa' },
+                        { label: 'Género', value: 'Masculino' }
+                    ]
+                }
             ],
             banner: "http://scd.es.rfi.fr/sites/espanol.filesrfi/dynimagecache/0/864/4896/2759/600/338/sites/images.rfi.fr/files/aef_image/img_20180313_1424497.jpg",
             date: '2018-05-12',
@@ -72,5 +51,5 @@ export default {
             date: '2018-04-07',
             firm: 'Esto es una firma'
         }
-    ],
+    ]
 }
--- src/services.js
+++ src/services.js
@@ -3,14 +3,25 @@ import ArticleModel from './models/ArticleModel'
 const ArticleServices = {
     fetchArticleList() {
         return new Promise((resolve) => {
+            const articleHeaders = fakeData.fakeFetchArticles.map(article => {
+                return {
+                    banner: article.banner,
+                    date: article.date,
+                    title: article.title,
+                    subTitle: article.subTitle,
+                    description: article.description
+                }
+            })
+
+            console.log(articleHeaders)
+
             setTimeout(() => {
-                resolve(fakeData.fakeFetchArticles)
+                resolve(articleHeaders)
             }, 2000)
         })
     },
     async fetchFullArticle(date, title) {
-
-        const findedFullArticle = fakeData.fakeFetchContent.find(article => article.date === date && article.title === title)
+        const findedFullArticle = fakeData.fakeFetchArticles.find(article => article.date === date && article.title === title)
         const fullArticleModeled = new ArticleModel(findedFullArticle)
         return fullArticleModeled
     }
